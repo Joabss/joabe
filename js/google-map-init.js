9de309ce60c5882753d4071a99cc7e41c0502046
Joabe <joabeo@gmail.com>
2025-04-02 12:37:21 -0300

    Adjustments II

--- js/google-map-init.js
+++ js/google-map-init.js
@@ -6,7 +6,7 @@ jQuery(document).ready(function() {
         var a = {
                 zoom: 11,
                 scrollwheel: false,
-                center: new google.maps.LatLng(40.67, -73.94),
+                center: new google.maps.LatLng(-23.577435081823126, -46.71562067748083),
                 styles: [{
                     "featureType": "landscape",
                     "stylers": [
@@ -127,7 +127,7 @@ jQuery(document).ready(function() {
             b = document.getElementById("map"),
             c = new google.maps.Map(b, a);
         new google.maps.Marker({
-            position: new google.maps.LatLng(40.67, -73.94),
+            position: new google.maps.LatLng(-23.577435081823126, -46.71562067748083),
             map: c,
             title: "Snazzy!"
         })
